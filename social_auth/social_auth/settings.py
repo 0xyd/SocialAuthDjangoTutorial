645018a0211ab07da9421ae9f98b615f6183a53c
davisfreeman1015 <davisfreeman1015@gmail.com>
2015-02-22 08:59:56 +0800

    Update

--- social_auth/social_auth/settings.py
+++ social_auth/social_auth/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
